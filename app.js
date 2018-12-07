a164085db813fd53164b42d2b1ca95d3fa1886a5
Shiyu Wang <shiyu3169@gmail.com>
2018-12-07 10:12:31 -0500

    hello wold

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path = require("path");
 
 const app = express();
 
+app.get("/", function(req, res) {
+    res.send("hello world");
+});
+
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
